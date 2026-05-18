5852bde4ac174ade9404bada66b44b3467d3e064
anishamalde <anishamalde95@gmail.com>
2026-05-18 10:36:46 +0100

    Upgrade to react native 0.83

--- packages/vega/test/App.spec.tsx
+++ packages/vega/test/App.spec.tsx
@@ -7,19 +7,28 @@
 import 'react-native';
 import {fireEvent, render} from '@testing-library/react-native';
 import * as React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
 
 import {App} from '../src/App';
 
 describe('Template App Snapshot tests', () => {
-  it('Initial App screen', () => {
-    const screen = render(<App />);
-    expect(screen).toMatchSnapshot();
+  it('Initial App screen', async () => {
+    let screen: ReturnType<typeof render>;
+    await ReactTestRenderer.act(async () => {
+      screen = render(<App />);
+    });
+    expect(screen!).toMatchSnapshot();
   });
 
-  it('App screen after link press', () => {
-    const screen = render(<App />);
-    const button = screen.getByTestId('sampleLink');
-    fireEvent.press(button);
-    expect(screen).toMatchSnapshot();
+  it('App screen after link press', async () => {
+    let screen: ReturnType<typeof render>;
+    await ReactTestRenderer.act(async () => {
+      screen = render(<App />);
+    });
+    const button = screen!.getByTestId('sampleLink');
+    await ReactTestRenderer.act(async () => {
+      fireEvent.press(button);
+    });
+    expect(screen!).toMatchSnapshot();
   });
 });
